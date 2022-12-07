cc3cbd7599231f0c2756b6412c214c1eeecc8e49
Anshuman Sarangi <anshumans005@gmail.com>
2022-12-08 01:17:17 +0530

    Patch 3(Fixed Login Form)

--- src/components/pageComponents/Login.jsx
+++ src/components/pageComponents/Login.jsx
@@ -1,42 +1,32 @@
 // import required modules
-import { React } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useRef } from "react";
+import { useState } from "react";
 
 // import and define Axios
-const axios = require("axios").default;
+import axios from "axios";
 
 function Login() {
-  const emailInputRef = useRef();
-  const passwordInputRef = useRef();
-
   // define useNavigate
   const navigate = useNavigate();
+  const [loginData, setLoginData] = useState({});
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
 
-    const enteredemail = emailInputRef.current.value; // .current.value will hold the currently entered value of the input element object
-    const enteredPassword = passwordInputRef.current.value;
+    const enteredEmail = event.target.email.value;
+    const enteredPassword = event.target.password.value;
 
-    const loginData = {
-      email: enteredemail,
-      password: enteredPassword,
-    };
+    // setLoginData({ email: enteredEmail, password: enteredPassword });
 
     // Axios sends data from this page to backend
 
-    const headers = {
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8",
-      },
-    };
     let url = `http://localhost:5000/api/v1/login`;
-    async function loginThisUser() {
-      const res = await axios.post(url, loginData, headers);
-      console.log("User Logged In", res);
-    }
-    loginThisUser();
+    const res = await axios.post(url, {
+      email: enteredEmail,
+      password: enteredPassword,
+    });
+    console.log("User Logged In");
   }
 
   return (
@@ -54,18 +44,18 @@ function Login() {
               className="w-full h-12 outline-none bg-[#92adbd] text-[#1a2834] font-medium placeholder:text-[#465059] border border-[#1a2834] rounded-md text-sm shadow-md px-5"
               required
               id="email"
+              name="email"
               type="email"
               placeholder="Email-Address"
-              ref={emailInputRef}
             ></input>
 
             <input
               className="w-full h-12 outline-none bg-[#92adbd] text-[#1a2834] font-medium placeholder:text-[#465059] border border-[#1a2834] rounded-md text-sm shadow-md px-5"
               required
               id="password"
+              name="password"
               type="password"
               placeholder="Password"
-              ref={passwordInputRef}
             ></input>
 
             <button className="inline-block px-6 py-2.5 bg-[#1a2834] text-[#92adbd] font-medium text-xs leading-tight uppercase rounded hover:bg-[#0a2f4f] active:bg-[#1a2834] transform hover:scale-105 active:scale-95 transition duration-300 ease-in-out">
